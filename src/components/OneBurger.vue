a337eb08ed5ab4e8244fb9699502671a9d5c78ee
juliushedell <julius.hedell.9951@gmail.com>
2023-11-09 13:51:20 +0100

    PLEASE HELP

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-      {{ burger.name }} {{ burger.kCal }}
-    </div>
+  <h4>{{ burger.name }}</h4>
+    <img v-bind:src="burger.url" class="images">
+      <ul>
+        <span class="ingredients">
+          <li>{{ burger.kCal }}</li>
+          <li>{{ burger.gluten }}</li>
+          <li>{{ burger.lactose }}</li>
+        </span>
+      </ul>
   </template>
   
   <script>
@@ -15,6 +21,19 @@
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
-  
+
+.images {
+  height: 15em;
+}
+
+burger.name {
+    text-transform: uppercase;
+    text-align: center;
+}
+
+.ingredients {
+    color: #ff0000;
+    font-weight: bold;
+}
   </style>
   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,72 @@
 <template>
-  <div>
-    <div>
-      <h1>Burgers</h1>
-      <Burger v-for="burger in burgers"
+  <header>
+         <img src="img/restaurant_ny.jpg" id="headpicture">
+         <h1>Welcome to Bashkan's Burgers!</h1>
+      </header>
+      <main>
+         <h3 id="menu">MENU</h3>
+         <p id="instructions">Here you can see the amazing burgers we provide.</p>
+         <section class="burgers">
+          <div>
+          <Burger v-for="burger in burgersall"
               v-bind:burger="burger" 
               v-bind:key="burger.name"/>
-    </div>
-    <div id="map" v-on:click="addOrder">
-      click here
-    </div>
-  </div>
+            </div>
+         </section>
+         <section id="contact">
+            <h3>
+               Order information:
+            </h3>
+            <form>
+               <p>
+                  <label for="fullname">Name</label><br>
+                  <input type="text" id="fullname" name="fn" required="required" placeholder="Full name">
+               </p>
+               <p>
+                  <label for="email">E-mail</label><br>
+                  <input type="email" id="email" name="em" required="required" placeholder="E-mail adress">
+               </p>
+               <p>
+                  <label for="adress">Street</label><br>
+                  <input type="text" id="address" name="ad" required="required" placeholder="Street name">
+               </p>
+               <p>
+                  <label for="house">House</label><br>
+                  <input type="number" id="house" name="ho" required="required" placeholder="House number">
+               </p>
+               <p>
+                  <label for="recipient">Payment method</label><br>
+                  <select id="recipient" name="rcp">
+                     <option>Debit card</option>
+                     <option>Credit card</option>
+                     <option selected="selected">Swish</option>
+                     <option>Klarna</option>
+                     <option>Paypal</option>
+                  </select>
+               </p>
+               <p>
+                  Please select gender<br>
+                  <label>
+                  <input type="radio" name="gender" value="Male"> Male
+                  </label><br>
+                  <label>
+                  <input type="radio" name="gender" value="Female"> Female
+                  </label><br>
+                  <label>
+                  <input type="radio" name="gender" value="Do not wish to provide" checked="checked"> Do not wish to provide
+                  </label>
+               </p>
+            </form>
+         </section>
+         <button type="submit">
+         <img src="img/Delivery.png" style="width: 1cm;">
+         Place order!
+         </button>
+      </main>
+      <hr>
+      <footer>
+         <p id="footer"> &copy; 2023 Bashkan's Burgers! All rights reserved. </p>
+      </footer>
 </template>
 
 <script>
@@ -18,6 +75,21 @@ import io from 'socket.io-client'
 
 const socket = io();
 
+function menuItem(nm, kc, url, gl, la){
+  this.name = nm;
+  this.kCal = kc;
+  this.url = url;
+  this.gluten = gl;
+  this.lactose = la;
+ }
+
+let burgersall = [
+  {name: "FATTYPATTY", kCal: 500, url:"img/Burger_1.jpeg", lactose: true, gluten: true}, 
+  {name: "American style", kCal: 1000, url: "img/Burger_2.jpeg", lactose: false, gluten: true},
+  {name: "EUW", kCal: 700, url: "img/Burger_3.jpeg", lactose: false, gluten: false}];
+
+console.log(burgersall)
+
 export default {
   name: 'HomeView',
   components: {
@@ -25,10 +97,7 @@ export default {
   },
   data: function () {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
-                 {name: "standard burger", kCal: 450},
-                 {name: "large burger", kCal: 850}
-               ]
+      burgers: burgersall
     }
   },
   methods: {
@@ -50,9 +119,87 @@ export default {
 </script>
 
 <style>
-  #map {
-    width: 300px;
-    height: 300px;
-    background-color: red;
-  }
+body {
+    background-color:rgb(189, 201, 212);
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+}
+/* ALLERGY INFORMATION*/
+.ingredients {
+    color: #ff0000;
+    font-weight: bold;
+}
+
+#burgernames {
+    text-transform: uppercase;
+    text-align: center;
+}
+
+header, main {
+    margin: 2em;
+}
+
+#menu, #instructions {
+    text-align: center;
+}
+
+.burgers {
+    border: #ff0000;
+    border-style: dashed;
+}
+
+#contact {
+    background-color: black;
+    color: white;
+    border: white;
+    border-style: dashed;
+    padding: 1em;
+}
+
+button:hover {
+    background-color: rgb(0, 98, 255);
+    color: white;
+    cursor: pointer;
+}
+
+button {
+    margin-top: 2em;
+    margin-left: 0.8em;
+}
+
+.images {
+    height: 15em;
+}
+
+section {
+    margin: 0.3em;
+}
+
+#footer {
+    font-size: small;
+}
+
+#headpicture {
+    width: 100%;
+    opacity: 0.3;
+}
+
+h1 {
+    position: absolute;
+    left: 0;
+    top: 0;
+    margin-top: 5em;
+    margin-left: 14em;
+}
+
+.wrapper {
+    display: grid;
+    grid-gap: 10px;
+    grid-template-columns: repeat(3, 1fr);
+}
+
+.box {
+    border-radius: 5px;
+    padding: 10px;
+    margin: auto;
+}
 </style>
